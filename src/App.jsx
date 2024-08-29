b4cc2b76531e140af36ea42ab57bdd9dffddf5a9
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-29 17:34:55 +0100

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/CreateOrder.jsx";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder.jsx";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
 
@@ -20,6 +22,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -28,10 +31,13 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
