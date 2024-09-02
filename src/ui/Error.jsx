f9d7562a783e168c05d597c151814e12f218316b
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-02 20:55:34 +0100

    okay

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -10,6 +10,8 @@ function Error() {
       <h1>Something went wrong 😢</h1>
       <p>%{error.data || error.message}%</p>
 
+      <LinkButton to="-1">&larr; Go back</LinkButton>
+
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
